7138a0b0adb2329070999f6a4e5780c0a357a50a
Jeff Schofield <jeff@jeffschofield.com>
2022-10-09 13:47:58 -0400

    chore: Add types for art asset virtual import

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+// Add virtual import types
+declare module 'virtual:art_assets' {
+  import { ArtAsset } from './types'
+
+  /** Art assets compiled from the `/src/assets/artwork` folder */
+  export const art_assets: ArtAsset[]
+}
