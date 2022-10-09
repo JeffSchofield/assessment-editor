19716c6bbdbf949b16cd9e45856b723ccea4d06a
Jeff Schofield <jeff@jeffschofield.com>
2022-10-09 13:49:46 -0400

    test: Add test for artwork asset context

---
+++ tests/contexts/asset.test.tsx
@@ -0,0 +1,17 @@
+﻿import { describe, expect, it } from 'vitest'
+import { renderHook } from '../utils'
+import { useArtAssets } from '../../src/contexts/assets'
+
+describe('Assets Contexts', () => {
+  describe('Art Assets Context', () => {
+    it('should provide an array of art assets', () => {
+      const { result } = renderHook(() => useArtAssets())
+
+      expect(Array.isArray(result.current)).toBeTruthy() // Make sure the hook returns an array
+      expect(result.current.length).toBeGreaterThan(0) // Make sure there is something in the array
+      expect(Object.keys(result.current[0])).toEqual(
+        expect.arrayContaining(['id', 'name', 'src', 'content', 'category_ids']) // Make sure the array contains an art asset object
+      )
+    })
+  })
+})
