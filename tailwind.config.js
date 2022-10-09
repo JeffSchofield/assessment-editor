c760b8ae1670e3bc6d053c83cf608ceb45212e74
Jeff Schofield <jeff@jeffschofield.com>
2022-10-09 19:49:54 -0400

    feat: Add color system to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
+const colors = require('./colors.config')
+
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,tsx}'],
   theme: {
+    colors: {
+      primary: colors.blue,
+      secondary: colors.indigo,
+      neutral: colors.zinc,
+      ...colors,
+      white: colors.zinc[50],
+      black: colors.zinc[900],
+      transparent: 'transparent',
+      current: 'currentColor',
+      inherit: 'inherit'
+    },
     extend: {}
   },
   plugins: [
