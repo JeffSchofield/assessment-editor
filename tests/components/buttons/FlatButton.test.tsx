2d991af7a72450366ae84d4ee18b3e8b9d2037b8
Jeff Schofield <jeff@jeffschofield.com>
2022-10-09 21:31:49 -0400

    feat: Add FlatButton component

---
+++ tests/components/buttons/FlatButton.test.tsx
@@ -0,0 +1,14 @@
+﻿import React from 'react'
+import { describe, expect, it } from 'vitest'
+import { render } from '../../utils'
+
+import { FlatButton } from '../../../src/components/buttons/FlatButton'
+
+describe('Flat Button Component', () => {
+  it('should display the text passed in as a child', () => {
+    const button_test_text = 'Test Content'
+    const button = render(<FlatButton>{button_test_text}</FlatButton>)
+
+    expect(button.baseElement).toHaveTextContent(button_test_text)
+  })
+})
