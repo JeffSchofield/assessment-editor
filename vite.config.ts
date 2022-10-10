7a6bd5a3b12c67e735f24a6d75443bb11821a55a
Jeff Schofield <jeff@jeffschofield.com>
2022-10-09 20:32:37 -0400

    feat: Add SVG icon spritesheet generator to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
 import react from '@vitejs/plugin-react'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import { assetsPlugin } from './src/lib/assets-plugin'
 
 export default defineConfig({
-  plugins: [react(), assetsPlugin()],
+  plugins: [
+    react(),
+    createSvgIconsPlugin({
+      iconDirs: [resolve(process.cwd(), 'src/assets/icons')]
+    }),
+    assetsPlugin()
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
