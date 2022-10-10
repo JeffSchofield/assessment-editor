a7f9fc1fec7f4e80a7dcf9197ffc2d133370c34e
Jeff Schofield <jeff@jeffschofield.com>
2022-10-10 16:31:41 -0400

    feat: Add EditorAssetSearchInput component

---
+++ src/components/editor/AssetSearchInput.tsx
@@ -0,0 +1,9 @@
+﻿import React from 'react'
+import { TextInput } from '../inputs/TextInput'
+
+/** Search input filter for art assets */
+export function EditorAssetSearchInput({
+  ...props
+}: React.InputHTMLAttributes<HTMLInputElement>) {
+  return <TextInput leftIcon="search" {...props} />
+}
