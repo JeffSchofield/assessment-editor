68a819b86ee405da336f0dd64abef14675fed0a6
Jeff Schofield <jeff@jeffschofield.com>
2022-10-10 15:19:05 -0400

    refactor: Update App to show the editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Editor } from './components/editor/Editor'
 
 function App() {
-  return <div>Hello World</div>
+  return <Editor data-testid="editor" />
 }
 
 export default App
