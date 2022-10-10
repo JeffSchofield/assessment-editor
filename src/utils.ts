a30ff5c5513e7a5030ebaed0f4ae4ab5c6e00224
Jeff Schofield <jeff@jeffschofield.com>
2022-10-10 18:45:37 -0400

    feat: Add utility to check if a stage object is an art asset stage object

---
+++ src/utils.ts
@@ -0,0 +1,8 @@
+﻿import { ArtAssetStageObject, StageObject, StageObjectType } from './types'
+
+/** Checks whether a stage object is specifically an art asset stage object. */
+export function isArtAssetStageObject(
+  object: StageObject
+): object is ArtAssetStageObject {
+  return object.type == StageObjectType.ART_ASSET
+}
