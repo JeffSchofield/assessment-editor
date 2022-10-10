9c55f4c4c69e67992dc0af820be44a08906540b8
Jeff Schofield <jeff@jeffschofield.com>
2022-10-10 09:51:03 -0400

    feat: Add PaneHeading component

---
+++ src/components/layout/PaneHeading.tsx
@@ -0,0 +1,14 @@
+﻿import React from 'react'
+
+interface PaneHeadingProps {
+  children?: React.ReactNode
+}
+
+/** Styled heading for a pane element. */
+export function PaneHeading({ children }: PaneHeadingProps) {
+  return (
+    <div className="p-1/2 text-sm uppercase font-medium bg-neutral-800">
+      {children}
+    </div>
+  )
+}
