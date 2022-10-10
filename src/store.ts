493a738223de960bf801aa6ec91087fb0ab61bd2
Jeff Schofield <jeff@jeffschofield.com>
2022-10-10 19:38:42 -0400

    feat: Add the centralized project store

---
+++ src/store.ts
@@ -0,0 +1,12 @@
+﻿import { configureStore } from '@reduxjs/toolkit'
+import project_reducer from './stores/project'
+
+// Create app-level store
+export const store = configureStore({
+  reducer: {
+    project: project_reducer
+  }
+})
+
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
