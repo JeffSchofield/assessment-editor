8da11fb638dd34b87a5a6b3b17b2f8cff4dba51a
Jeff Schofield <jeff@jeffschofield.com>
2022-10-10 21:51:03 -0400

    feat: Click to add asset and build stage content from project state

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -1,8 +1,13 @@
-﻿import React from 'react'
+import React from 'react'
 import { PaneHeading } from '../layout/PaneHeading'
 import { EditorAssetPane } from './AssetPane'
 import { Stage, Layer, Rect } from 'react-konva'
-import { useAppSelector } from '../../hooks'
+import { useAppDispatch, useAppSelector } from '../../hooks'
+import { isArtAssetStageObject } from '../../utils'
+import { EditorArtAssetStageObject } from './objects/ArtAssetStageObject'
+import { useArtAssets } from '../../contexts/assets'
+import { ArtAsset, ArtAssetStageObject, StageObjectType } from '../../types'
+import { addObject } from '../../stores/project'
 
 /**
  * Creates an instance of the visual editor.
@@ -12,6 +17,44 @@ export function Editor({
   ...props
 }: React.HtmlHTMLAttributes<HTMLDivElement>) {
   const project = useAppSelector(state => state.project)
+  const dispatch = useAppDispatch()
+  const art_assets = useArtAssets()
+
+  /**
+   * Asset pane item selection handling
+   */
+  function handleAssetClick(asset: ArtAsset) {
+    dispatch(
+      addObject({
+        type: StageObjectType.ART_ASSET,
+        x: 0,
+        y: 0,
+        scale: { x: 1, y: 1 },
+        rotation: 0,
+        asset_id: asset.id
+      } as Omit<ArtAssetStageObject, 'id'>)
+    )
+  }
+
+  /**
+   * Build stage content
+   */
+  const stage_content = project.stage.map(stage_object => {
+    if (isArtAssetStageObject(stage_object)) {
+      return (
+        <EditorArtAssetStageObject
+          key={stage_object.id}
+          x={stage_object.x}
+          y={stage_object.y}
+          scale={stage_object.scale}
+          rotation={stage_object.rotation}
+          art_asset={art_assets.find(
+            asset => asset.id == stage_object.asset_id
+          )}
+        />
+      )
+    }
+  })
 
   return (
     <div className={className + ' h-full flex'} {...props}>
@@ -19,7 +62,7 @@ export function Editor({
       <div className="flex flex-col w-14 bg-neutral-850">
         {/* Assets Pane */}
         <PaneHeading>Assets</PaneHeading>
-        <EditorAssetPane className="flex-1" />
+        <EditorAssetPane className="flex-1" onAssetClick={handleAssetClick} />
       </div>
 
       {/* Main Viewport */}
@@ -37,7 +80,7 @@ export function Editor({
               fill="white"
             />
           </Layer>
-          <Layer data-testid="project-content-layer"></Layer>
+          <Layer data-testid="project-content-layer">{stage_content}</Layer>
         </Stage>
       </div>
     </div>
