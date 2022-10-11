8d7aa47b669ffd82d6d81bb1177cea514e76e3d3
Jeff Schofield <jeff@jeffschofield.com>
2022-10-11 19:56:26 -0400

    feat: Add save button to editor which exports stage as PNG

--- src/utils.ts
+++ src/utils.ts
@@ -6,3 +6,20 @@ export function isArtAssetStageObject(
 ): object is ArtAssetStageObject {
   return object.type == StageObjectType.ART_ASSET
 }
+
+/**
+ * Downloads a URI automatically.
+ *
+ * Credit: https://stackoverflow.com/questions/3916191/download-data-url-file/15832662
+ */
+export function downloadURI(uri: string, filename: string) {
+  // Create an anchor element setup to download the URI
+  const link = document.createElement('a')
+  link.download = filename
+  link.href = uri
+
+  // Add anchor to body, click it to trigger download, then remove
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
+}
