790d9aa79be33176d5867cfcce1590ea8620b865
Jeff Schofield <jeff@jeffschofield.com>
2022-10-11 21:06:44 -0400

    fix: Fix ts compile errors due to broken expect type extensions

---
+++ tests/test-env.d.ts
@@ -0,0 +1,11 @@
+﻿import 'vitest'
+
+declare global {
+  namespace Vi {
+    /* eslint-disable */
+    interface Assertion<T = any> {
+      toHaveTextContent(text: string): void
+      toHaveValue(value: any): void
+    }
+  }
+}
